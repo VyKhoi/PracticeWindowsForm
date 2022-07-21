81124218f537d9654c48ef1081eb1bbe5793f39e
Dang Vy Khoi <89650460+VyKhoi@users.noreply.github.com>
2022-07-22 01:59:47 +0700

    Add files via upload

---
+++ SimpleEditor/Editor/Editor/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sizeFont = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.red = new System.Windows.Forms.HScrollBar();
+            this.blue = new System.Windows.Forms.HScrollBar();
+            this.green = new System.Windows.Forms.HScrollBar();
+            this.mixValueColor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(232, 564);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(280, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(510, 439);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 464);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Size";
+            // 
+            // sizeFont
+            // 
+            this.sizeFont.Location = new System.Drawing.Point(333, 464);
+            this.sizeFont.Name = "sizeFont";
+            this.sizeFont.Size = new System.Drawing.Size(100, 26);
+            this.sizeFont.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(449, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "B";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(530, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "/";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(601, 462);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "U";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(264, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(264, 541);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(264, 576);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // red
+            // 
+            this.red.Location = new System.Drawing.Point(376, 502);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(243, 26);
+            this.red.TabIndex = 10;
+            this.red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.red_Scroll);
+            // 
+            // blue
+            // 
+            this.blue.Location = new System.Drawing.Point(376, 535);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(243, 26);
+            this.blue.TabIndex = 11;
+            this.blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.red_Scroll);
+            // 
+            // green
+            // 
+            this.green.Location = new System.Drawing.Point(376, 570);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(243, 26);
+            this.green.TabIndex = 12;
+            this.green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.red_Scroll);
+            // 
+            // mixValueColor
+            // 
+            this.mixValueColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.mixValueColor.Location = new System.Drawing.Point(650, 507);
+            this.mixValueColor.Name = "mixValueColor";
+            this.mixValueColor.Size = new System.Drawing.Size(140, 89);
+            this.mixValueColor.TabIndex = 13;
+            this.mixValueColor.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 628);
+            this.Controls.Add(this.mixValueColor);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sizeFont);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sizeFont;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.HScrollBar red;
+        private System.Windows.Forms.HScrollBar blue;
+        private System.Windows.Forms.HScrollBar green;
+        private System.Windows.Forms.Label mixValueColor;
+    }
+}
+
---
+++ SimpleEditor/Editor/Editor/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
+using System.Drawing.Text;
+
+namespace Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // get full name font fami
+            InstalledFontCollection col = new InstalledFontCollection();
+            foreach (FontFamily fa in col.Families)
+            {
+                listBox1.Items.Add(fa.Name);
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new 
+                Font(listBox1.SelectedItem.ToString(), float.Parse(sizeFont.Text));
+         
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if  (richTextBox1.SelectionFont.Bold == false)
+            {
+                richTextBox1.SelectionFont = new Font("",
+                    richTextBox1.SelectionFont.Size, FontStyle.Bold);
+            }
+            else
+            {
+                richTextBox1.SelectionFont = new Font("",
+                  richTextBox1.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont.Italic == false)
+            {
+                richTextBox1.SelectionFont = new Font("",
+                    richTextBox1.SelectionFont.Size, FontStyle.Italic);
+            }
+            else
+            {
+                richTextBox1.SelectionFont = new Font("",
+                  richTextBox1.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont.Underline == false)
+            {
+                richTextBox1.SelectionFont = new Font("",
+                    richTextBox1.SelectionFont.Size, FontStyle.Underline);
+            }
+            else
+            {
+                richTextBox1.SelectionFont = new Font("",
+                  richTextBox1.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void red_Scroll(object sender, ScrollEventArgs e)
+        {
+            Color a = Color.FromArgb(red.Value, green.Value, blue.Value);
+            mixValueColor.BackColor = a;
+            richTextBox1.SelectionColor = a;
+        }
+    }
+}
