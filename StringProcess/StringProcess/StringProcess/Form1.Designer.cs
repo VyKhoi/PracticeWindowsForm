b4084fed5203caf467e248ae521652d1af65d44f
Dang Vy Khoi <89650460+VyKhoi@users.noreply.github.com>
2022-07-22 02:01:34 +0700

    Add files via upload

---
+++ StringProcess/StringProcess/StringProcess/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace StringProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.s1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.s3 = new System.Windows.Forms.TextBox();
+            this.s2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.indexS1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(194, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chuong trinh minh hoa xu ly chuoi";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chuổi 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chuổi 2";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // s1
+            // 
+            this.s1.Location = new System.Drawing.Point(141, 100);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(687, 26);
+            this.s1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(495, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Chuổi 3";
+            // 
+            // s3
+            // 
+            this.s3.Location = new System.Drawing.Point(607, 145);
+            this.s3.Name = "s3";
+            this.s3.Size = new System.Drawing.Size(250, 26);
+            this.s3.TabIndex = 5;
+            // 
+            // s2
+            // 
+            this.s2.Location = new System.Drawing.Point(141, 145);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(250, 26);
+            this.s2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(408, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Thay thế s2 bằng s3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(607, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Đảo từ trong s1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(408, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Xóa s2 trong s1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(607, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Chuẩn hóa chuổi ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.indexS1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "chèn s2 vào s1";
+            // 
+            // indexS1
+            // 
+            this.indexS1.Location = new System.Drawing.Point(7, 26);
+            this.indexS1.Name = "indexS1";
+            this.indexS1.Size = new System.Drawing.Size(54, 26);
+            this.indexS1.TabIndex = 0;
+            this.indexS1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.indexS1_KeyDown);
+            this.indexS1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.indexS1_KeyPress);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(101, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 50);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Chèn";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "vị trí";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(866, 71);
+            this.panel1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 340);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.s3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox s1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox s3;
+        private System.Windows.Forms.TextBox s2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox indexS1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ StringProcess/StringProcess/StringProcess/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (s1.Text.Length > Convert.ToInt16(indexS1.Text))
+            {
+                s1.Text = s1.Text.Insert(Convert.ToInt16(indexS1.Text), s2.Text);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Left < panel1.Width)
+            {
+                label1.Left += 5;
+            }
+            else
+            {
+                label1.Left = -250;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(s1.Text.Contains(s2.Text))
+            {
+                s1.Text =  s1.Text.Replace(s2.Text, s3.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[] CharStr = s1.Text.ToArray();
+            Array.Reverse(CharStr);
+
+            s1.Text = new string(CharStr);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (s1.Text.Contains(s2.Text))
+            {
+                s1.Text = s1.Text.Remove(s1.Text.IndexOf(s2.Text), s2.Text.Length);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            string[] arrStr = s1.Text.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+                //Split(new char[] { ' ', ',', '.' });
+            foreach(string i in arrStr)
+            {
+                //i.Trim();
+                str += i + " ";
+               // MessageBox.Show(i);
+            }
+            s1.Text = str;
+        }
+
+        private void indexS1_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+        }
+
+        private void indexS1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if( !char.IsDigit( e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
