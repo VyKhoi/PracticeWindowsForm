22ac26630a7ab9e1135010a700b9168c61d00e7c
Dang Vy Khoi <89650460+VyKhoi@users.noreply.github.com>
2022-07-22 01:51:43 +0700

    Add files via upload

---
+++ LiftTheBall/LiftTheBall/LiftTheBall/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LiftTheBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LiftTheBall.Properties.Resources.s2;
+            this.pictureBox1.Location = new System.Drawing.Point(199, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(82, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 501);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LiftTheBall/LiftTheBall/LiftTheBall/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiftTheBall
+{
+    public partial class Form1 : Form
+    {
+        int x =5;
+        int y = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if( pictureBox1.Top < 0 || pictureBox1.Bounds.IntersectsWith(label1.Bounds))
+            {
+                y = -y;
+                //if(pictureBox1.Bottom == label1.Top)
+                //{
+                //    y = -y;
+                //}
+
+            }
+            if (pictureBox1.Left < 0 || pictureBox1.Right > this.Width || pictureBox1.Bounds.IntersectsWith(label1.Bounds))
+            {
+                x = -x;
+            }
+            if(pictureBox1.Bottom > this.Height)
+            {
+                timer1.Enabled = false;
+              //  timer1.Enabled = false;
+
+                MessageBox.Show("you lose");
+                this.Close();
+               // Application.Exit();
+            }
+            pictureBox1.Top += y;
+            pictureBox1.Left += x;
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+           // if(e.Bu)
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            { 
+                case Keys.Left:
+                {
+                        if (label1.Left > 0)
+                        {
+                            label1.Left -= 10;
+                            
+                        }
+                    break;
+                }
+                case Keys.Right:
+                    {
+                        if (label1.Right < this.Width)
+                        {
+                            label1.Left += 10;
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
